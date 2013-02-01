6ded8e896bc2f3f8e8fe7063bb5390a404e45c95
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-02-01 17:20:22 +0900

    add: first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OctopusValidator::Application.config.secret_token = '574873b45d77db2bcbd34512d4d5b80409a6c41e6622fead9c725b58f061700e66665d60ec937525be2e58a528400efe0c64015c6063747671cebb92df6086ee'
