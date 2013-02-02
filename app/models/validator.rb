e56ef00c7bd1b776cb91c70c3b20f838a3a850eb
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-02-02 16:31:42 +0900

    add: Validator#priority

--- app/models/validator.rb
+++ app/models/validator.rb
@@ -1,3 +1,10 @@
 class Validator < ActiveRecord::Base
-  attr_accessible :content, :format, :name
+  attr_accessible :content, :format, :name, :priority
+  default_scope order('priority ASC')
+
+  after_initialize :initialize_priority
+
+  def initialize_priority
+    self.priority ||= Validator.count + 1
+  end
 end
