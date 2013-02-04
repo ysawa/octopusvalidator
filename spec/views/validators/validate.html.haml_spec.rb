082572c52a0c52128acddfbadc2d0eb60aa78127
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-02-04 16:35:18 +0900

    add: tests for view of validators/validate

--- spec/views/validators/validate.html.haml_spec.rb
+++ spec/views/validators/validate.html.haml_spec.rb
@@ -4,9 +4,28 @@ require 'spec_helper'
 describe "validators/validate" do
   before(:each) do
     @validator = Fabricate(:validator)
+    view
   end
 
-  it "renders" do
-    render
+  context 'without any params' do
+    before :each do
+      assign(:href, nil)
+    end
+
+    it "renders" do
+      render
+    end
+  end
+
+
+  context 'with params' do
+    before :each do
+      assign(:href, 'http://example.com' )
+    end
+
+    it "renders" do
+      render
+      rendered.should have_content 'http://example.com'
+    end
   end
 end
